0545c4ce865c319386f64051eb12501a7a30eadf
remadyt <insomom98@yandex.ru>
2021-04-28 14:30:48 +0300

    HW7 done

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import HW7 from "../../h7/HW7";
 
 const Junior = () => {
     return (
         <div>
-            HW6-11
+            <HW7 />
+            {/*<HW8 />*/}
+            {/*<HW9 />*/}
+            {/*<HW10 />*/}
+            {/*<HW1 />*/}
         </div>
     );
 };
--- src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -16,7 +16,8 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // onChange, onChangeOption
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
     }
 
 
@@ -24,7 +25,10 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
         <label key={name + '-' + i}>
             <input
                 type={'radio'}
-                // name, checked, value, onChange
+                onChange={onChangeCallback}
+                value={o}
+                checked={o === value}
+                name={name}
             />
             {o}
         </label>
